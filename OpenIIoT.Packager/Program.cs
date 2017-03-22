bf27442e87b0428c6e3e68e0cc45c4e6f0407ce0
JP Dillingham <jpdillingham@gmail.com>
2017-03-21 22:16:59 -0500

    Implemented DIY command line parser

--- OpenIIoT.Packager/Program.cs
+++ OpenIIoT.Packager/Program.cs
@@ -11,6 +11,34 @@ namespace OpenIIoT.Packager
     {
         private static void Main(string[] args)
         {
+            Dictionary<string, string> parsedArgs = ParseArgs(Environment.CommandLine);
+
+            foreach (string key in parsedArgs.Keys)
+            {
+                Console.WriteLine("Key: " + key + " value: " + parsedArgs[key]);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, string> ParseArgs(string args)
+        {
+            Dictionary<string, string> retVal = new Dictionary<string, string>();
+
+            string regEx = "(?:[-]{1,2}|\\/)([\\w-]+)[=|:]?(\\w\\S*|\\\".*\\\"|\\\'.*\\\')?";
+
+            foreach (Match match in Regex.Matches(String.Join(" ", args), regEx))
+            {
+                if (match.Groups.Count == 3)
+                {
+                    if (!retVal.ContainsKey(match.Groups[1].Value))
+                    {
+                        retVal.Add(match.Groups[1].Value, match.Groups[2].Value);
+                    }
+                }
+            }
+
+            return retVal;
         }
     }
 }
